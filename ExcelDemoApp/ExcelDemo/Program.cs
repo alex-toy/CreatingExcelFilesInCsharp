68fefe3bac45708956dcfedcbca9e3f6a7f1eee6
alex-toy <alexei.80@hotmail.fr>
2022-12-13 09:54:45 +0100

    export

---
+++ ExcelDemoApp/ExcelDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Data;
+
+namespace ExcelDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\source\CsharpLibraries\CreatingExcelFilesInCsharp\sampleData.xlsx";
+            SpreadsheetDocument spreadSheetDocument = SpreadsheetDocument.Create(filePath, SpreadsheetDocumentType.Workbook);
+
+            WorkbookPart workbookPart = spreadSheetDocument.AddWorkbookPart();
+            workbookPart.Workbook = new Workbook();
+
+            WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+            worksheetPart.Worksheet = new Worksheet(new SheetData());
+            Sheets sheets = spreadSheetDocument.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets());
+            Sheet sheet = new Sheet()
+            {
+                Id = spreadSheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                SheetId = 1,
+                Name = "Sample Data"
+            };
+
+            sheets.Append(sheet);
+
+            Worksheet worksheet = worksheetPart.Worksheet;
+            SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+
+            for(int i = 0; i < 5; i++)
+            {
+                Row row = new Row();
+
+                for(int j = 0; j < 5; j++)
+                {
+                    Cell cell = new Cell()
+                    {
+                        CellValue = new CellValue((i * j).ToString()),
+                        DataType = CellValues.String
+                    };
+                    row.Append(cell);
+                }
+
+                sheetData.Append(row);
+            }
+
+            worksheetPart.Worksheet.Save();
+
+            spreadSheetDocument.Close();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Creating Excel Files In C#
 
 Spreadsheets are an easy way to visualize and manipulate data that almost any user can operate. In this project, we will see how to output data to Excel, format the document, and then also read data from Excel back into C#.
+
+## Export
+
+### Add Nuget Packages
+```
+Install-Package Open-XML-SDK
+```
+
+<img src="/pictures/export.png" title="export"  width="800">
